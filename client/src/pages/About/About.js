339eec93cce1923f2c8a55d016147e4965f5962b
JamilW <jamilweeks@yahoo.com>
2019-06-20 21:51:06 -0400

    Improved shadow effects

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -16,9 +16,9 @@ function About() {
               </div>
             </div> */}
           
-            <div className="card" style={{fontSize:"12px", backgroundColor: "#3b3a30", color:"white", boxShadow: "10px 10px black", transform: "translateY(4px)"}}>
-              <div className="img-container-fluid" >
-                <img alt="jw" className="card-img-top-fluid" src={jw} style={{objectFit: "cover"}}></img> 
+            <div className="card" style={{fontSize:"12px", backgroundColor: "#3b3a30", boxShadow: "10px 10px 2px black", transform: "translateY(4px)", color:"white"}}>
+              <div className="img-container-fluid">
+                <img alt="jw" className="card-img-top-fluid" src={jw} style={{objectFit: "cover", paddingTop: "30px", paddingBottom: "30px"}}></img> 
               </div>
               <div className="card-body" style={{color: "#ffef96", background: "#3b3a30", paddingTop: "30px", textAlign: "center", height: "77px"}}>
               <b><p><em>Click Contact above to connect.</em></p></b>
@@ -26,7 +26,7 @@ function About() {
             </div>
           </div>
           <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8" id="aboutMe" style={{paddingBottom: "20px"}}>
-            <p style={{fontSize:"14px", padding: "10px", color: "#ffef96", boxShadow: "10px 10px black", transform: "translateY(4px)", background: "rgb(65, 62, 62)", marginLeft: "200px"}}>
+            <p style={{fontSize:"14px", padding: "10px", color: "#ffef96", background: "rgb(65, 62, 62)", boxShadow: "10px 10px 2px black", transform: "translateY(4px)", marginLeft: "200px"}}>
             <b><em>
             Web Developer with a passion for using my natural ability for creativity alongside my technical expertise
             in HTML, CSS, and Javascript to work in a team friendly environment. Honing my research skills from my 
@@ -35,7 +35,7 @@ function About() {
             in an office environment, best demonstrated by helping my team become the number one team in the nation in 
             sales through developing weekly competitive projects for my team to attain monthly goals.</em></b>
               </p>
-              <p style={{fontSize:"14px", padding: "10px", color: "#ffef96", boxShadow: "10px 10px black", transform: "translateY(4px)", background: "rgb(65, 62, 62)", marginLeft: "200px"}}>
+              <p style={{fontSize:"14px", padding: "10px", color: "#ffef96", boxShadow: "10px 10px 2px black", transform: "translateY(4px)", background: "rgb(65, 62, 62)", marginLeft: "200px"}}>
               <b>Education:<br/><br/>
               Full stack web development from UNC at Chapel Hill, NC<br/>
               <em>2018-2019</em><br/><br/>
