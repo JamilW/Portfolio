caf70b683798a1ea78a61061b12b9069567c4b58
JamilW <jamilweeks@yahoo.com>
2019-08-01 11:00:14 -0400

    added blank targets to contact links

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -7,10 +7,10 @@ function Contact() {
         <h4 className="page-header" style={{fontFamily: "'Cinzel', serif"}}><b>Contact Information</b><hr/></h4>
           <ul class="list-group">
             <li class="list-group-item"><b>E-Mail:</b><br/><br/> jamilw5@gmail.com</li><br/>
-            <li class="list-group-item"><b>Github:</b><br/><br/> <a href="https://github.com/JamilW" style={{textDecoration: "underline"}}>Link</a></li><br/>
-            <li class="list-group-item"><b>LinkedIn:</b><br/><br/> <a href="https://www.linkedin.com/in/jamil-weeks-72703315a/" style={{textDecoration: "underline"}}>Link</a></li><br/>
+            <li class="list-group-item"><b>Github:</b><br/><br/> <a href="https://github.com/JamilW" target="blank" style={{textDecoration: "underline"}}>Link</a></li><br/>
+            <li class="list-group-item"><b>LinkedIn:</b><br/><br/> <a href="https://www.linkedin.com/in/jamil-weeks-72703315a/" target="blank" style={{textDecoration: "underline"}}>Link</a></li><br/>
             <li class="list-group-item"><b>Phone Number:</b><br/><br/> (910) 922-3044</li><br/>
-            <li class="list-group-item"><b>Resume:</b><br/><br/> <a href="https://hephophome.files.wordpress.com/2019/03/professional-resume.docx" style={{textDecoration: "underline"}}>Link</a></li>
+            <li class="list-group-item"><b>Resume:</b><br/><br/> <a href="https://hephophome.files.wordpress.com/2019/03/professional-resume.docx" target="blank" style={{textDecoration: "underline"}}>Link</a></li>
           </ul>	 
       </div>  
     )
